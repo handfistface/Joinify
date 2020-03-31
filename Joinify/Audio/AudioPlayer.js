4da8fe1fdeb6d0320a12910e7dc93065b94e07a7
John Kirschner <handfistface@gmail.com>
2020-03-31 17:21:37 -0400

    Project now is now functional at a basic level.

--- Joinify/Audio/AudioPlayer.js
+++ Joinify/Audio/AudioPlayer.js
@@ -23,26 +23,25 @@ class AudioPlayer {
         if (this.isAudioReady == false)
             return;     //the player is already playing audio
 
-        //connect to the channel that the user has just connected to
-        const channel = this.client.channels.cache.get(this.channelId);
-        channel.join().then(connection => {
-            //voice chat join successful
-            this.isAudioReady = false;
-            //'D:\code\Joinify\Joinify\AudioFiles\yooooooooooo.mp3'
-            var ytStream = ytdl(link);
-            const dispatcher = connection.play(ytStream, { volume: 0.25, quality: 'highestaudio' })
-            //dispatcher.on('error', (ex) => {
-            //    console.error('Error while playing audio: ' + ex)
-            //});
-            .on('end', () => {
-                //ended playing the file
-                this.isAudioReady = true;
-                if (this.leaveChatAfterPlay == true)
-                    channel.leave();
+        try {
+            //connect to the channel that the user has just connected to
+            const channel = this.client.channels.cache.get(this.channelId);
+            channel.join().then(connection => {
+                //voice chat join successful
+                this.isAudioReady = false;
+                //'D:\code\Joinify\Joinify\AudioFiles\yooooooooooo.mp3'
+                var ytStream = ytdl(link, { filter: 'audioonly' });
+                const dispatcher = connection.play(ytStream, { volume: 0.25, quality: 'highestaudio' })
+                dispatcher.on('finish', () => {
+                    //ended playing the file
+                    this.isAudioReady = true;
+                    if (this.leaveChatAfterPlay == true)
+                        channel.leave();
+                });
             });
-        }).catch(ex => {
-            console.error('Error connecting to voice channel: ' + ex);
-        });
+        } catch (ex) {
+            console.error('Exception while playing audio in AudioPlayer: ' + ex);
+        }
     }
 
 }
--- Joinify/README.md
+++ Joinify/README.md
@@ -1,3 +1,3 @@
 # Joinify
 
-
+A discord bot which watches channels and will join a channel when a user joins and plays audio. 
